97dfa545cf5989c53c09cfff180b4992a7cc8249
Ashan <ashanvimodh241@gmail.com>
2025-02-28 04:54:10 +0530

    Learned about reusing components

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+const names = ['Ashan', 'Romie']
+
+function Post() {
+
+
+    return (
+        <di>
+            <p>Ashan</p>
+            <p>Romie is awesome!</p>
+        </di>
+    );
+}
+
+export default Post;
