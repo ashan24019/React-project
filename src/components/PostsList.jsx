8fa5342b145dcb146197f8b48e1caeedff440fad
Ashan <ashanvimodh241@gmail.com>
2025-02-28 16:33:48 +0530

    Working with state and Lifting State up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,25 @@
 import Post from "./Post";
 import classes from './PostsList.module.css'
 import NewPost from "./NewPost";
+import { useState } from "react";
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost/>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
             <ul className={classes.posts}>
-                <Post author="Theo" body="My name is Ashan"/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Romie" body="My name is Harsha"/>
             </ul>
         </>
