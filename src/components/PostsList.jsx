9b4b6753f9e6dc672ced50e4274e25741d3d8e63
Ashan <ashanvimodh241@gmail.com>
2025-03-08 07:39:17 +0530

    Updating Satate based on previos State

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import { useState } from "react";
 import Modal from "./Modal";
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
+  const [post,setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPost) => [postData, ...existingPost]);
+  }
 
   return (
     <>
       {isPosting ? (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onCancel = {onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
         </Modal>
-      ) : false }
+      ) : (
+        false
+      )}
 
       <ul className={classes.posts}>
         <Post author="Romie" body="My name is Harsha" />
