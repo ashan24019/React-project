45e256d88f2997e5678a94f9604f729df3cf6b8f
Ashan <ashanvimodh241@gmail.com>
2025-03-10 13:57:19 +0530

    complete react crash project

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import Posts, {loader as postLoader} from "./routes/Posts.jsx";
 import NewPost, {action as newPostAction} from "./routes/NewPost.jsx";
 import RootLayout from "./routes/RootLayout.jsx";
+import PostDetails, {loader as postDetailsLoader} from "./routes/PostDetails.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Posts />,
         loader: postLoader,
-        children: [{ path: "/create-post", element: <NewPost />, action: newPostAction}],
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction},
+          {path: '/:id', element: <PostDetails/>, loader: postDetailsLoader}
+        ],
       },
     ],
   },
